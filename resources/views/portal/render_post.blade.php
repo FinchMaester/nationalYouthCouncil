7dd5782eb7d772e51ad8fd4de1dffd68972aea6c
NIharikaGhimire <niharikaghimire29@gmail.com>
2023-03-22 10:26:34 +0545

    fixed title of render post blade

--- resources/views/portal/render_post.blade.php
+++ resources/views/portal/render_post.blade.php
@@ -4,7 +4,7 @@
 {{-- <div class="container">
 
     <div class="row mt-3">
-       <h4 style="text-align: center;">{{ $post->title }}</h4>
+        <h4 style="text-align: center;">{{ $post->title }}</h4>
 
 
         <div class="">
@@ -12,7 +12,7 @@
 
                 <img src=" {{ asset('uploads/posts/' . $post->image ?? '')   }}" style="height:100px;">
                 <div class="card-body">
-                   {!! $post->content !!}
+                    {!! $post->content !!}
 
                 </div>
             </div>
@@ -25,8 +25,8 @@
                 <a class="card-wel-title" href="{{ route('render_posts', $list->slug) }}">
                     <div class="list_title">
                         <li>{{ $list->title }}
-                    </li>
-                </div>
+                        </li>
+                    </div>
                 </a>
                 @endforeach
             </div>
@@ -38,62 +38,63 @@
 </div> --}}
 
 
-    <div class="container py-5">
-        <div class="row mb-12">
-            <div class="col-lg-12">
-                <h4>{{ $post->title }}</h4>
-            </div>
+<div class="container py-5">
+    <div class="row mb-12">
+        <div class="col-lg-12">
+            <h3 class="sec_title">{{ !empty($post->title) ? $post->title:'' }}</h3>
         </div>
+    </div>
 
-        <div class="single-post-container">
-            <div class="row ">
+    <div class="single-post-container">
+        <div class="row ">
 
-                <div class="col-md-offset-1 col-sm-12 col-md-12 col-md-10 ">
+            <div class="col-md-offset-1 col-sm-12 col-md-12 col-md-10 ">
 
-                    <div class="post-list">
+                <div class="post-list">
 
-                        <div class="row ">
-                            <div class="col-sm-6 col-md-8 col-md-8 ">
-                                <div class="post" style="visibility: visible;">
-                                    <div class="text-block">
-                                        <img class="mt-3 service-img" src="{{ asset('uploads/posts/' . $post->image ?? '') }}" style="width: 50%;"
-                                            alt="">
-                                        <div class="post-text mt-5">{!! $post->content !!} </div>
-                                    </div>
+                    <div class="row ">
+                        <div class="col-sm-6 col-md-8 col-md-8 ">
+                            <div class="post" style="visibility: visible;">
+                                <div class="text-block">
+                                    <img class="mt-3 service-img"
+                                        src="{{ asset('uploads/posts/' . $post->image ?? '') }}" style="width: 50%;"
+                                        alt="">
+                                    <div class="post-text mt-5">{!! $post->content !!} </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
 
-{{--
-            <div class="other_posts my-4 py-4">
-                <h5>Other Posts</h5>
-                <ul class="list_title">
-                    @foreach ($postslist as $list )
-                    <li>
-                        <a href="{{ route('render_posts', $list->slug) }}">{{ $list->title }}</a>
-                    </li>
-                    @endforeach
+        {{--
+        <div class="other_posts my-4 py-4">
+            <h5>Other Posts</h5>
+            <ul class="list_title">
+                @foreach ($postslist as $list )
+                <li>
+                    <a href="{{ route('render_posts', $list->slug) }}">{{ $list->title }}</a>
+                </li>
+                @endforeach
 
-                </ul>
-            </div> --}}
+            </ul>
+        </div> --}}
 
-            <div class="col-md-3">
-                <div class="card-wel">
+        <div class="col-md-3">
+            <div class="card-wel">
                 <h5 class="title_card">Other Posts</h5>
                 @foreach ($postslist as $list )
-                        <a class="card-wel-title" href="{{ route('render_posts', $list->slug) }}">
-                            <li>{{ $list->title }}</li>
-                        </a>
-                    @endforeach
+                <a class="card-wel-title" href="{{ route('render_posts', $list->slug) }}">
+                    <li>{{ $list->title }}</li>
+                </a>
+                @endforeach
 
-                </div>
             </div>
-
         </div>
+
     </div>
+</div>
 
 
 
